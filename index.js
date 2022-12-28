1bb3a8b421b0d822005f2e41b7508489c3431bf3
Lestrades <lestrades@gmail.com>
2022-12-28 19:50:12 +0100

    diseño 5

---
+++ index.js
@@ -0,0 +1,29 @@
+const enemy01 = document.getElementById("enemy01");
+const enemy02 = document.getElementById("enemy02");
+const enemy03 = document.getElementById("enemy03");
+const enemy04 = document.getElementById("enemy04");
+const enemy05 = document.getElementById("enemy05");
+const enemy06 = document.getElementById("enemy06");
+const enemy07 = document.getElementById("enemy07");
+const enemy08 = document.getElementById("enemy08");
+const enemy09 = document.getElementById("enemy09");
+const enemy10 = document.getElementById("enemy10");
+const enemy11 = document.getElementById("enemy11");
+const enemy12 = document.getElementById("enemy12");
+const btnCrear = document.getElementById("btnCrear");
+const btnMatar = document.getElementById("btnMatar");
+const btnLanzar = document.getElementById("btnLanzar");
+const btnCantidad = document.getElementById("btnCantidad");
+const btnLados = document.getElementById("btnLados");
+const resultado = document.getElementById("resultado");
+const btnCantMas = document.getElementById("btnCantMas");
+const btnCantMenos = document.getElementById("btnCantMenos");
+const btnLadoMas = document.getElementById("btnLadoMas");
+const btnLadoMenos = document.getElementById("btnLadoMenos");
+
+//resultado.innerText = "Hola"
+
+
+//enemy01.style.background = "black";
+// enemy01.src = "images/x.svg";
+// enemy01.src.fill = "red";
